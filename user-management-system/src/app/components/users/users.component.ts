e5223fd34609060d1882b52f387cebb8762d9cad
proactivecode91 <proactivecoder91@gmail.com>
2023-05-17 12:00:05 +0530

    feat : displayed results from the get api in the user componentof the application

--- user-management-system/src/app/components/users/users.component.ts
+++ user-management-system/src/app/components/users/users.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
-
+export class UsersComponent implements OnInit {
+  users$!:Observable<any>;
+  constructor(private _userService: UserService){}
+  ngOnInit(): void {
+     this.users$= this._userService.getUsers()
+  }
 }
