6b9d93a91df90c4adaf4c7546f4891b65e62040e
proactivecode91 <proactivecoder91@gmail.com>
2023-05-17 11:40:10 +0530

    user component added and route configured

--- user-management-system/src/app/app.module.ts
+++ user-management-system/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
